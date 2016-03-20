7301309463c954d1bf0b4484a31ff44cb2e0045f
Nick King <penguin@frozendesert.net>
2016-03-19 21:18:15 -0700

    Added something resembling a setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+        name='slackminion',
+        version='0.1.0',
+        packages=['slackminion', 'slackminion.plugins'],
+        url='https://github.com/arcticfoxnv/slackminion',
+        license='MIT',
+        author='',
+        author_email='',
+        description='',
+        install_requires=[
+            'bottle',
+            'requests',
+            'Rocket',
+            'six',
+            'slackclient',
+            'websocket-client',
+        ],
+        entry_points={
+            'console_scripts': [
+                'slackminion = slackminion.__main__:main',
+            ]
+        }
+)
---
+++ slackminion/__main__.py
@@ -0,0 +1,17 @@
+import logging
+
+from config import *
+from slackminion.bot import Bot
+
+
+def main():
+    level = logging.DEBUG if DEBUG else logging.INFO
+    logging.basicConfig(level=level, format='%(asctime)s %(name)s %(levelname)s: %(message)s')
+
+    bot = Bot(SLACK_TOKEN)
+    bot.start()
+    bot.run()
+    bot.stop()
+
+if __name__ == "__main__":
+    main()
