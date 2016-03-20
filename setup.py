183cf8619f900ca281eb3279d2034c286923851e
Nick King <penguin@frozendesert.net>
2016-03-20 10:45:34 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='slackminion',
-        version='0.1.0',
+        version='0.1.1',
         packages=['slackminion', 'slackminion.plugins'],
         url='https://github.com/arcticfoxnv/slackminion',
         license='MIT',
