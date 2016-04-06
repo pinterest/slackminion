62699046e89b42c03031089d6866acd2b2675eec
Nick King <penguin@frozendesert.net>
2016-04-06 06:21:22 -0700

    Fix crash when message has no user

--- slackminion/bot.py
+++ slackminion/bot.py
@@ -141,9 +141,10 @@ class Bot(object):
         return resp['channel']['id']
 
     def _load_user_rights(self, user):
-        if 'bot_admins' in self.config:
-            if user.username in self.config['bot_admins']:
-                user.is_admin = True
+        if user is not None:
+            if 'bot_admins' in self.config:
+                if user.username in self.config['bot_admins']:
+                    user.is_admin = True
 
     def _handle_event(self, event):
         if 'type' not in event:
