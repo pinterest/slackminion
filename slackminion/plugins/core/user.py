d4eed04859f8e398ffa12e1f4c0de01a2aef73fc
Nick King <penguin@frozendesert.net>
2016-04-25 19:28:01 -0700

    If user is not an admin, set flag to false

--- slackminion/plugins/core/user.py
+++ slackminion/plugins/core/user.py
@@ -57,3 +57,5 @@ class UserManager(BasePlugin):
         """Sets permissions on user object"""
         if user.username in self.admins:
             user.is_admin = True
+        elif not hasattr(user, 'is_admin'):
+            user.is_admin = False
