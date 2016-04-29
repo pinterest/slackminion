ce006b07615d65e700fa033f8a6be639c5df3c6d
Nick King <penguin@frozendesert.net>
2016-04-29 04:53:38 -0700

    Update BasePlugin.get_user to use UserManager if available

--- slackminion/plugin/base.py
+++ slackminion/plugin/base.py
@@ -81,9 +81,16 @@ class BasePlugin(object):
         """
         Utility function to query slack for a particular user
 
-        :param username: The username or userid of the user to lookup
+        :param username: The username of the user to lookup
         :return: SlackUser object or None
         """
+        if hasattr(self._bot, 'user_manager'):
+            user = self._bot.user_manager.get_by_username(username)
+            if user:
+                return user
+            user = SlackUser.get_user(self._bot.sc, username)
+            self._bot.user_manager.set(user)
+            return user
         return SlackUser.get_user(self._bot.sc, username)
 
     def get_channel(self, channel):
