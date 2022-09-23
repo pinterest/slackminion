0894b95a89b9f32df3ce083c1410eb5f411028f6
Henrique Fagundes <hfagundes@pinterest.com>
2022-09-23 14:43:33 -0300

    Bump slack_sdk minor version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "MarkupSafe>=0.23",
         "PyYAML>=4.2b",
         "requests >=2.11, <3.0a0",
-        "slack_sdk==3.13.0",
+        "slack_sdk==3.18.3",
         "websocket-client >=0.35, <0.55.0",
         "Werkzeug>=0.10.4",
         "aiohttp>=3.7.4.post0",
